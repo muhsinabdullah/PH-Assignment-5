995691c9d1c5b935923b855836c7c43f0455142b
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-08-29 00:47:50 +0600

    Call Buttons and Call History Funtionality add

--- index.js
+++ index.js
@@ -27,3 +27,71 @@ heartButtonIds.forEach(function(id){
         heartCount.textContent = count;
     });
 });
+
+// Call Buttons Functions
+const coinCount = document.getElementById('coin-count');
+
+let coins = 100;
+
+const callHistoryContainer = document.getElementById('call-history');
+
+const callButtonIds = [
+    "call-btn-1",
+    "call-btn-2",
+    "call-btn-3",
+    "call-btn-4",
+    "call-btn-5",
+    "call-btn-6",
+    "call-btn-7",
+    "call-btn-8",
+    "call-btn-9"
+]
+
+const services =[
+    {name: 'National Emergency Number', number: '999'},
+    {name: 'Police Helpline Number', number: '999'},
+    {name: 'Fire Service Number', number: '999'},
+    {name: 'Ambulance Service', number: '1994-999999'},
+    {name: 'Women & Child Helpline', number: '109'},
+    {name: 'Anti-Corruption Helpline', number: '106'},
+    {name: 'Electricity Helpline', number: '16216'},
+    {name: 'Brac Helpline', number: '16445 '},
+    {name: 'Bangladesh Railway Helpline ', number: '163'},
+];
+
+callButtonIds.forEach(function(id, index){
+    const btn = document.getElementById(id);
+
+    btn.addEventListener('click', function(){
+        const service = services[index];
+
+        if (coins < 20){
+            alert('Not Enough Coins To Make This Call');
+            return;
+        }
+    coins -= 20;
+    coinCount.textContent = coins;
+
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+
+    const historyItem = document.createElement('div');
+    historyItem.style.display = 'flex';
+    historyItem.style.justifyContent = 'space-between';
+    historyItem.style.borderBottom = '1px solid #ccc';
+    historyItem.style.padding = '5px 0'
+    
+    const left = document.createElement('div');
+    left.innerHTML = `
+    <strong>${service.name}</strong> <br>
+    <strong>${service.number}</strong>
+    `
+    const right = document.createElement('div');
+    right.textContent= time;
+
+    historyItem.appendChild(left);
+    historyItem.appendChild(right);
+
+    callHistoryContainer.appendChild(historyItem);
+    })
+})
