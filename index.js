1fb924b866aa4b5e12b528e1538ddd206b0d841c
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-08-28 22:57:22 +0600

    Heart Button Functionality Add

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("I am Connected")
+// Heart Button Count Function
+const heartCount = document.getElementById('heart-count');
+
+let count = 0;
+
+const heartButtonIds = [
+    'heart-btn-1',
+    'heart-btn-2',
+    'heart-btn-3',
+    'heart-btn-4',
+    'heart-btn-5',
+    'heart-btn-6',
+    'heart-btn-7',
+    'heart-btn-8',
+    'heart-btn-9'
+]
+heartButtonIds.forEach(function(id){
+    const btn = document.getElementById(id);
+    btn.addEventListener("click", function(){
+        if (btn.style.color === 'red'){
+            btn.style.color = '';
+            count--;
+        }else{
+            btn.style.color = 'red';
+            count++;
+        }
+        heartCount.textContent = count;
+    });
+});
